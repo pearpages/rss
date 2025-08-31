22a72e6dc5d99add76ae770f9dc4ca1e5c53f2fe
Pere Pages <pere.pages@ocado.com>
2025-08-31 10:35:16 +0200

    refactor: comment feeeds

--- src/config/feeds.ts
+++ src/config/feeds.ts
@@ -6,26 +6,26 @@ export const DEFAULT_RSS_FEEDS: RSSFeed[] = [
     url: 'https://feeds.bbci.co.uk/news/rss.xml',
     category: 'News'
   },
-  {
-    name: 'Hacker News',
-    url: 'https://hnrss.org/frontpage',
-    category: 'Technology'
-  },
+  // {
+  //   name: 'Hacker News',
+  //   url: 'https://hnrss.org/frontpage', // from: https://news.ycombinator.com/news
+  //   category: 'Technology'
+  // },
   {
     name: 'The Guardian',
     url: 'https://www.theguardian.com/international/rss',
     category: 'News'
   },
-  {
-    name: 'Dev.to',
-    url: 'https://dev.to/feed',
-    category: 'Development'
-  },
-  {
-    name: 'NASA News',
-    url: 'https://www.nasa.gov/rss/dyn/breaking_news.rss',
-    category: 'Science'
-  },
+  // {
+  //   name: 'Dev.to',
+  //   url: 'https://dev.to/api/articles?top=10', // Requires proxying because RSS2JSON doesn't want it
+  //   category: 'Development'
+  // },
+  // {
+  //   name: 'NASA News',
+  //   url: 'https://www.nasa.gov/rss/dyn/breaking_news.rss',
+  //   category: 'Science'
+  // },
   {
     name: 'Al Jazeera',
     url: 'https://www.aljazeera.com/xml/rss/all.xml',
